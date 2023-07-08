08a4d36dbfaa7bf2758a7861335d7641f77fb151
realdaly <mohammed96218@yahoo.com>
2023-07-08 17:03:21 +0300

    added books to homepage and books page

--- lms/lms_app/models.py
+++ lms/lms_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    status_choices = [
+        ("متوفر", "متوفر"),
+        ("مستعار", "مستعار"),
+        ("مباع", "مباع")
+    ]
+
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250, null=True, blank=True)
+    img = models.ImageField(upload_to="images", null=True, blank=True)
+    author_img = models.ImageField(upload_to="images", null=True, blank=True)
+    pages = models.IntegerField(null=True, blank=True)
+    price = models.IntegerField(null=True, blank=True)
+    active = models.BooleanField(default=True)
+    status = models.CharField(max_length=50, choices=status_choices, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
--- lms/lms_app/views.py
+++ lms/lms_app/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    context = {}
+    context = {
+        "books": Book.objects.all(),
+        "categories": Category.objects.all()
+    }
     return render(request, "pages/index.html", context)
+
+def books(request):
+    context = {
+        "books": Book.objects.all(),
+        "categories": Category.objects.all()
+    }
+    return render(request, "pages/books.html", context)
